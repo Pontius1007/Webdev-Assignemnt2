339f3520e7760c34847cf00898b1eb29342a5dc6
andrefei <andreas.eikeland@hotmail.com>
2017-09-07 12:16:04 +0200

    Added hover to button and removed redundant css

--- grateful.js
+++ grateful.js
@@ -13,7 +13,7 @@ function main() {
     //Add method for preloading GIFs?
 
     var change_var = function changeGif(){
-      var gif = document.getElementById('cat_gif');
+      var gif = document.getElementById('center-gif');
       i++;
       gif.src = gif_list[(i) % (gif_list.length)];
     }
